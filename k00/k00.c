36aba0655e886b130c2670759da11595f31044bd
bb35319053 <bb35319053@ms.nagasaki-u.ac.jp>
2021-01-23 00:24:58 +0900

    @

--- k00/k00.c
+++ k00/k00.c
@@ -1,18 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double x)
+{
+    return (x*x-2);
+}
+
 int main()
 {
     double x, xn, fd, h;
     x = 2.0;
     h = 0.0001;
     while(1) {
-    printf("x=%lf¥n",x);
-    fd = (f(x+h)-f(x))/h;
-    xn = x - f(x)/fd;
+        printf("x = %lf\n",x);
+        fd = (f(x+h)-f(x))/h;
+        xn = x - f(x)/fd;
         if(fabs(xn-x) < 0.00001){
-        break;
-        } else {
-        x = xn;
+            break;
+            } else {
+            x = xn;
         }
     }
-    printf("answer=%lf¥",xn);
+    printf("answer=%lf\n",xn);
     return 0;
 }
---
+++ k99/k01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double x, xn, fd, h;
+    x = 2.0;
+    h = 0.0001;
+    while(1) {
+    printf("x=%lf¥n",x);
+    fd = (f(x+h)-f(x))/h;
+    xn = x - f(x)/fd;
+        if(fabs(xn-x) < 0.00001){
+        break;
+        } else {
+        x = xn;
+        }
+    }
+    printf("answer=%lf¥",xn);
+    return 0;
+}
